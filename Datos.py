abe831e4c8fc35d5c8cd4096e1b3d8593e3a6235
Foxyctg <39318687+Foxyctg@users.noreply.github.com>
2021-07-13 04:22:19 +0200

    ci: :rocket: Se añaden los archivos

---
+++ Datos.py
@@ -0,0 +1,13 @@
+print("Indica tu nombre")
+nombre = input()
+
+print("Indica tu edad")
+edad = input()
+
+print("Indica tu genero")
+genero = input()
+
+print("Indica tu orientación")
+orientación = input()
+
+print("Ahora tu nombre es" , nombre , "tienes" , edad , "años" , "eres" , genero , "y te gustan los" , orientación)
---
+++ Hello.py
@@ -0,0 +1,14 @@
+foxy = "patata"
+x = 5
+x /= 3
+print(foxy)
+print(x)
+
+s1 = "Hola yo soy foxy\n"
+print(type(s1))
+
+print(s1 * 5)
+print("intruduce tu nombre")
+a = input()
+
+print("Ahora tu nombre sera " + a)
