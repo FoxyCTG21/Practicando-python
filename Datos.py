97cfec4b53ea12c214e9d72701225c9faa94f5ae
Foxyctg <39318687+Foxyctg@users.noreply.github.com>
2021-07-16 02:13:39 +0200

    feat(Prueba 1): :fire: Añadimos formato

--- Datos.py
+++ Datos.py
@@ -1,13 +1,27 @@
 print("Indica tu nombre")
+
 nombre = input()
 
 print("Indica tu edad")
+
 edad = input()
 
 print("Indica tu genero")
+
 genero = input()
 
 print("Indica tu orientación")
+
 orientación = input()
 
-print("Ahora tu nombre es" , nombre , "tienes" , edad , "años" , "eres" , genero , "y te gustan los" , orientación)
+print(
+    "Ahora tu nombre es",
+    nombre,
+    "tienes",
+    edad,
+    "años",
+    "eres",
+    genero,
+    "y te gustan los",
+    orientación,
+)
